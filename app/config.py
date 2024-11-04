8488fdedc040105ae903df8294dabca7a4a8e437
Brian Schneider de Lima <bschneiderlima@gmail.com>
2024-11-04 15:38:27 -0800

    Alteracao em config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123456@localhost/ponto')
+    # SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123456@localhost/ponto')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
