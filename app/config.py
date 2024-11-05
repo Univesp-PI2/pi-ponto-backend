a2a5e7a59dc82de6f9f4cd2adfb28acfb0a43f00
Brian Schneider de Lima <bschneiderlima@gmail.com>
2024-11-04 16:16:37 -0800

    Debugando string de conexão

--- app/config.py
+++ app/config.py
@@ -4,7 +4,5 @@ class Config:
     # SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret')
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:123456@localhost/ponto')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    print(SECRET_KEY)
-    print(os.getenv('DATABASE_URL'))
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
